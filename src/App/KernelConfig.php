0c45621f24deba6aea8758c1142cebbe1f2685c4
Mariusz Miłejko <mariusz@milejko.pl>
2015-09-20 22:06:49 +0200

    refactor

---
+++ src/App/KernelConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Mmi Framework (https://github.com/milejko/mmi.git)
+ * 
+ * @link       https://github.com/milejko/mmi.git
+ * @copyright  Copyright (c) 2010-2014 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
+ */
+
+namespace App;
+
+/**
+ * Ogólna konfiguracja aplikacji
+ */
+class KernelConfig extends \Cms\App\KernelConfig {
+	
+}
