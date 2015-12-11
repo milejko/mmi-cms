2ebe86984afabce3c79f43b881ac288b0e85e4aa
Maciej Pawłowski <mac@macu.eu>
2015-12-11 16:50:11 +0100

    walidator pustego cmsfile'a dla obiektu

---
+++ src/Cms/Validator/EmptyCmsFiles.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Mmi Framework (https://github.com/milejko/mmi.git)
+ * 
+ * @link       https://github.com/milejko/mmi.git
+ * @copyright  Copyright (c) 2010-2015 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
+ */
+
+namespace Cms\Validator;
+
+/**
+ * Walidator liczba pomiędzy
+ * 
+ * @method self setObject($object) ustawia obiekt
+ * @method self setObjectId($objectId) ustawia ID obiektu
+ * @method self setMessage($message) ustawia własną wiadomość walidatora
+ * 
+ * @method string getObject() pobiera obiekt
+ * @method integer getObjectId() pobiera ID obiektu
+ * @method string getMessage() pobiera wiadomość 
+ */
+class EmptyCmsFiles extends \Mmi\Validator\ValidatorAbstract {
+
+	/**
+	 * Komunikat błędnego kodu zabezpieczającego
+	 */
+	const INVALID = 'Proszę przesłać pliki';
+
+	/**
+	 * Ustawia opcje
+	 * @param array $options
+	 * @return self
+	 */
+	public function setOptions(array $options = [], $reset = false) {
+		return $this->setObject(current($options))
+				->setObjectId(next($options))
+				->setMessage(next($options));
+	}
+
+	/**
+	 * Waliduje czy w cms_file znajdują się jakieś pliki dla danego rekordu
+	 * @param string $value
+	 * @return boolean
+	 */
+	public function isValid($value) {
+		$query = (new \Cms\Orm\CmsFileQuery)
+			->byObject($this->getObject(), $this->getObjectId())
+			->find();
+		if ($query->count() === 0) {
+			$this->_error(self::INVALID);
+			return false;
+		}
+		return true;
+	}
+
+}
