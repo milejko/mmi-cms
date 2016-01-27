6a7d81e002dc82c7209a7f15484502996ed78c98
Maciej Pawłowski <mac@macu.eu>
2016-01-27 08:49:26 +0100

    annual update

--- src/Cms/Command/CronExecute.php
+++ src/Cms/Command/CronExecute.php
@@ -5,7 +5,7 @@
  * Mmi Framework (https://github.com/milejko/mmi.git)
  * 
  * @link       https://github.com/milejko/mmi.git
- * @copyright  Copyright (c) 2010-2015 Mariusz Miłejko (http://milejko.com)
+ * @copyright  Copyright (c) 2010-2016 Mariusz Miłejko (http://milejko.com)
  * @license    http://milejko.com/new-bsd.txt New BSD License
  */
 
