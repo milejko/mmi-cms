c1a4b8e26326e2bc9e3bb44c48da9c7a54b4e8df
Maciej Pawłowski <mac@macu.eu>
2016-04-12 10:41:31 +0200

    ldap validator

---
+++ src/Cms/Validator/LdapAccount.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Cms\Validator;
+
+/**
+ * Walidator konta w katalogu LDAP
+ */
+class LdapAccount extends \Mmi\Validator\ValidatorAbstract {
+
+	/**
+	 * Treść wiadomości
+	 */
+	const INVALID = 'Użytkownik nie istnieje w LDAP';
+
+	/**
+	 * Walidacja znaków alfanumerycznych
+	 * @param mixed $value wartość
+	 * @return boolean
+	 */
+	public function isValid($value) {
+		//sprawdzanie istnienia pojedynczego użytkownika o podanej nazwie
+		if (count((new \Cms\Model\Auth())->ldapAutocomplete($value)) != 1) {
+			$this->_error(self::INVALID);
+			return false;
+		}
+		//poprawne
+		return true;
+	}
+
+}
