55673261f26bff0cfd8d087e86700f2933a97aa2
Mariusz Miłejko <mariusz@milejko.pl>
2016-07-04 18:14:47 +0200

    attribute fixes

--- src/Cms/Orm/CmsAttributeGroupAttributeRecord.php
+++ src/Cms/Orm/CmsAttributeGroupAttributeRecord.php
@@ -7,9 +7,6 @@ class CmsAttributeGroupAttributeRecord extends \Mmi\Orm\Record {
 	public $id;
 	public $cmsAttributeId;
 	public $cmsAttributeGroupId;
-	public $required;
-	public $unique;
-	public $materialized;
 	public $active;
 
 }
