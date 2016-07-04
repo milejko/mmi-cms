9537c0c9b95909a5ce636ce1384602544307e3bf
Mariusz Miłejko <mariusz@milejko.pl>
2016-07-04 18:14:10 +0200

    attribute fixes

--- src/Cms/Orm/CmsAttributeRecord.php
+++ src/Cms/Orm/CmsAttributeRecord.php
@@ -12,8 +12,6 @@ class CmsAttributeRecord extends \Mmi\Orm\Record {
 	public $fieldClass;
 	public $filterClasses;
 	public $validatorClasses;
-	public $multiple;
-	public $restricted;
 	public $indexWeight;
 	public $required;
 	public $unique;
