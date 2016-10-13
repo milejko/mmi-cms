66ea82e4bf32c03f0386d38996cacff14313c1ed
Mariusz Miłejko <mariusz@milejko.pl>
2016-10-13 21:29:55 +0200

    fixed sql

--- src/Cms/Resource/incremental/mysql/0021_cms_attribute_relation.sql
+++ src/Cms/Resource/incremental/mysql/0021_cms_attribute_relation.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `cms_attribute_relation`
-ADD `order` int(11) NOT NULL DEFAULT '0' AFTER `objectId`,
+ADD `order` int(11) NOT NULL DEFAULT '0' AFTER `objectId`;
 ALTER TABLE `cms_attribute_relation`
 ADD INDEX `order` (`order`);
