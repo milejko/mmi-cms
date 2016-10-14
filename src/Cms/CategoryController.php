bfa3d4f7568772cafb1c78d31cec23060c406a1e
Mariusz Miłejko <mariusz@milejko.pl>
2016-10-14 14:29:44 +0200

    category fixes (publication dates) category refactor (smaller methods)

--- src/Cms/CategoryController.php
+++ src/Cms/CategoryController.php
@@ -19,67 +19,19 @@ class CategoryController extends \Mmi\Mvc\Controller {
 	 * Akcja dispatchera kategorii
 	 */
 	public function dispatchAction() {
-		//wyszukanie kategorii
-		if ((null === $category = (new Model\CategoryModel)
-			->getCategoryByUri($this->uri))) {
-			//404
-			throw new \Mmi\Mvc\MvcNotFoundException('Category not found: ' . $this->uri);
-		}
-		//kategoria nieaktywna i brak roli redaktora treści		
-		if ($category->active != 1 && (
-			$category->dateStart > date('Y-m-d H:i:s') || ($category->dateEnd < date('Y-m-d H:i:s') && !is_null($category->dateEnd))
-			) && !\App\Registry::$acl->isAllowed(\App\Registry::$auth->getRoles(), 'cmsAdmin:category:index')
-		) {
-			//404
-			throw new \Mmi\Mvc\MvcNotFoundException('Category not found: ' . $this->uri);
-		}
-
-		//kategoria nieaktywna i brak roli redaktora treści
-		if ($category->active == 1 &&
-			$category->dateStart < date('Y-m-d H:i:s') &&
-			$category->dateEnd > date('Y-m-d H:i:s') &&
-			!\App\Registry::$acl->isAllowed(\App\Registry::$auth->getRoles(), 'cmsAdmin:category:index')) {
-			//404
-			throw new \Mmi\Mvc\MvcNotFoundException('Category not found: ' . $this->uri);
-		}
+		//pobranie kategorii
+		$category = $this->_getPublishedCategoryByUri($this->uri);
 		//kategoria posiada customUri, a wejście jest na natywny uri
 		if ($category->customUri && $this->uri == $category->uri) {
 			//przekierowanie na customUri
 			$this->getResponse()->redirect('cms', 'category', 'dispatch', ['uri' => $category->customUri]);
 		}
-		//tworzenie nowego requestu na podstawie obecnego
-		$request = clone $this->getRequest();
-		$request->setModuleName('cms')
-			->setControllerName('category')
-			->setActionName('article');
-		//pobranie typu i ustalenie template
-		if ($category->getJoined('cms_category_type')->template != '') {
-			//tablica z tpl
-			$mcaArr = explode('/', $category->getJoined('cms_category_type')->template);
-			//zła ilość argumentów
-			if (count($mcaArr) != 3) {
-				throw new \Exception('Template invalid: "' . $category->getJoined('cms_category_type')->template . '"');
-			}
-			//ustawienie request
-			$request->setModuleName($mcaArr[0])
-				->setControllerName($mcaArr[1])
-				->setActionName($mcaArr[2]);
-		}
-		//iteracja po dzieciach kategorii
-		foreach ($category->getOption('parents') as $cat) {
-			//brak widoczności w menu
-			if (!$cat->active) {
-				continue;
-			}
-			//dodawanie okruszka
-			$this->view->navigation()->appendBreadcrumb($cat->name, $this->view->url(['uri' => $cat->uri]), $cat->title ? $cat->title : $cat->name, $cat->description ? $cat->description : $cat->lead);
-		}
-		//dodawanie okruszka z kategorią główną
-		$this->view->navigation()->appendBreadcrumb($category->name, $this->view->url(['uri' => $category->uri]), $category->title ? $category->title : $category->name, $category->description ? $category->description : $category->lead);
+		//tworzy parametry SEO
+		$this->_buildSeoParams($category);
 		//model widgetu do widoku
 		$this->view->widgetModel = new Model\CategoryWidgetModel($category->id);
 		//forward do akcji docelowej
-		return \Mmi\Mvc\ActionHelper::getInstance()->forward($request);
+		return \Mmi\Mvc\ActionHelper::getInstance()->forward($this->_prepareForwardRequest($category));
 	}
 
 	/**
@@ -92,6 +44,88 @@ class CategoryController extends \Mmi\Mvc\Controller {
 		$this->view->attributes = (new Model\AttributeValueRelationModel('category', $category->id))->getAttributeValues();
 		//przekazanie tagów
 		$this->view->tags = (new Model\TagRelationModel('cmscategory', $category->id))->getTagRelations();
+	}
+
+	/**
+	 * Pobiera opublikowaną kategorię po uri
+	 * @param string $uri
+	 * @return \Cms\Orm\CmsCategoryRecord
+	 * @throws \Mmi\Mvc\MvcNotFoundException
+	 */
+	private function _getPublishedCategoryByUri($uri) {
+		//wyszukanie kategorii
+		if ((null === $category = (new Model\CategoryModel)
+			->getCategoryByUri($uri))) {
+			//404
+			throw new \Mmi\Mvc\MvcNotFoundException('Category not found: ' . $uri);
+		}
+		//kategoria dozwolona - rola redaktora
+		if (\App\Registry::$acl->isAllowed(\App\Registry::$auth->getRoles(), 'cmsAdmin:category:index')) {
+			return $category;
+		}
+		//kategoria manualnie wyłączona
+		if (!$category->active) {
+			//404
+			throw new \Mmi\Mvc\MvcNotFoundException('Category not found: ' . $uri);
+		}
+		//nie osiągnięto czasu publikacji
+		if (null !== $category->dateStart && $category->dateStart > date('Y-m-d H:i:s')) {
+			//404
+			throw new \Mmi\Mvc\MvcNotFoundException('Category not found: ' . $uri);
+		}
+		//przekroczono czas publikacji
+		if (null !== $category->dateEnd && $category->dateEnd < date('Y-m-d H:i:s')) {
+			//404
+			throw new \Mmi\Mvc\MvcNotFoundException('Category not found: ' . $uri);
+		}
+		//opublikowana kategoria
+		return $category;
+	}
+
+	/**
+	 * Pobiera request do przekierowania
+	 * @param \Cms\Orm\CmsCategoryRecord $category
+	 * @return \Mmi\Http\Request
+	 * @throws \Mmi\App\KernelException
+	 */
+	private function _prepareForwardRequest(\Cms\Orm\CmsCategoryRecord $category) {
+		//tworzenie nowego requestu na podstawie obecnego
+		$request = clone $this->getRequest();
+		$request->setModuleName('cms')
+			->setControllerName('category')
+			->setActionName('article');
+		//pobranie typu i ustalenie template
+		if (!$category->getJoined('cms_category_type')->template) {
+			return $request;
+		}
+		//tablica z tpl
+		$mcaArr = explode('/', $category->getJoined('cms_category_type')->template);
+		//zła ilość argumentów
+		if (count($mcaArr) != 3) {
+			throw new \Mmi\App\KernelException('Template invalid: "' . $category->getJoined('cms_category_type')->template . '"');
+		}
+		//ustawienie request
+		return $request->setModuleName($mcaArr[0])
+				->setControllerName($mcaArr[1])
+				->setActionName($mcaArr[2]);
+	}
+
+	/**
+	 * Buduje parametry SEO
+	 * @param \Cms\Orm\CmsCategoryRecord $category
+	 */
+	private function _buildSeoParams(\Cms\Orm\CmsCategoryRecord $category) {
+		//iteracja po dzieciach kategorii
+		foreach ($category->getOption('parents') as $cat) {
+			//brak widoczności w menu
+			if (!$cat->active) {
+				continue;
+			}
+			//dodawanie okruszka
+			$this->view->navigation()->appendBreadcrumb($cat->name, $this->view->url(['uri' => $cat->uri]), $cat->title ? $cat->title : $cat->name, $cat->description ? $cat->description : $cat->lead);
+		}
+		//dodawanie okruszka z kategorią główną
+		$this->view->navigation()->appendBreadcrumb($category->name, $this->view->url(['uri' => $category->uri]), $category->title ? $category->title : $category->name, $category->description ? $category->description : $category->lead);
 	}
 
 }
