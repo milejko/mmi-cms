cc6bcad7ae974a043905bb70a3ba09c07474c4e9
Mariusz Miłejko <mariusz@milejko.pl>
2016-11-06 18:56:44 +0100

    improved default attribute value management

--- src/Cms/Orm/CmsAttributeRelationRecord.php
+++ src/Cms/Orm/CmsAttributeRelationRecord.php
@@ -2,13 +2,21 @@
 
 namespace Cms\Orm;
 
+/**
+ * Rekord relacji atrybutu
+ */
 class CmsAttributeRelationRecord extends \Mmi\Orm\Record {
 
 	public $id;
 	public $cmsAttributeId;
+	
+	/**
+	 * Wartość domyślna
+	 * @var mixed
+	 */
 	public $cmsAttributeValueId;
 	public $object;
 	public $objectId;
 	public $order;
-
+	
 }
