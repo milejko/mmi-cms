21d2edef78211aaea5d83ad26584f943c9ba81ef
Mariusz Miłejko <mariusz@milejko.pl>
2016-11-18 11:34:38 +0100

    fix attribute types

---
+++ src/Cms/Resource/incremental/mysql/0029_attribute_type_fix.sql
@@ -0,0 +1,11 @@
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Cms\\Form\\Element\\DatePicker' WHERE `id` = 1;
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Cms\\Form\\Element\\DateTimePicker' WHERE `id` = 2;
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Cms\\Form\\Element\\Tags' WHERE `id` = 3;
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Mmi\\Form\\Element\\Text' WHERE `id` = 4;
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Mmi\\Form\\Element\\Textarea' WHERE `id` = 5;
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Cms\\Form\\Element\\TinyMce' WHERE `id` = 6;
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Mmi\\Form\\Element\\File' WHERE `id` = 7;
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Cms\\Form\\Element\\Plupload' WHERE `id` = 8;
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Mmi\\Form\\Element\\Checkbox' WHERE `id` = 9;
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Mmi\\Form\\Element\\Select' WHERE `id` = 10;
+UPDATE `cms_attribute_type` SET `fieldClass` = '\\Mmi\\Form\\Element\\MultiCheckbox' WHERE `id` = 11;
