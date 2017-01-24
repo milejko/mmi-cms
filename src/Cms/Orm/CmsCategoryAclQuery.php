b91c0be82533cf15d440b172437b2be7048280e7
Mariusz Miłejko <mariusz@milejko.pl>
2017-01-24 18:23:54 +0100

    category acl orm

---
+++ src/Cms/Orm/CmsCategoryAclQuery.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Cms\Orm;
+
+//<editor-fold defaultstate="collapsed" desc="CmsCategoryAclQuery">
+/**
+ * @method CmsCategoryAclQuery limit($limit = null)
+ * @method CmsCategoryAclQuery offset($offset = null)
+ * @method CmsCategoryAclQuery orderAsc($fieldName, $tableName = null)
+ * @method CmsCategoryAclQuery orderDesc($fieldName, $tableName = null)
+ * @method CmsCategoryAclQuery andQuery(\Mmi\Orm\Query $query)
+ * @method CmsCategoryAclQuery whereQuery(\Mmi\Orm\Query $query)
+ * @method CmsCategoryAclQuery orQuery(\Mmi\Orm\Query $query)
+ * @method CmsCategoryAclQuery resetOrder()
+ * @method CmsCategoryAclQuery resetWhere()
+ * @method QueryHelper\CmsCategoryAclQueryField whereId()
+ * @method QueryHelper\CmsCategoryAclQueryField andFieldId()
+ * @method QueryHelper\CmsCategoryAclQueryField orFieldId()
+ * @method CmsCategoryAclQuery orderAscId()
+ * @method CmsCategoryAclQuery orderDescId()
+ * @method CmsCategoryAclQuery groupById()
+ * @method QueryHelper\CmsCategoryAclQueryField whereCmsRoleId()
+ * @method QueryHelper\CmsCategoryAclQueryField andFieldCmsRoleId()
+ * @method QueryHelper\CmsCategoryAclQueryField orFieldCmsRoleId()
+ * @method CmsCategoryAclQuery orderAscCmsRoleId()
+ * @method CmsCategoryAclQuery orderDescCmsRoleId()
+ * @method CmsCategoryAclQuery groupByCmsRoleId()
+ * @method QueryHelper\CmsCategoryAclQueryField whereCmsCategoryId()
+ * @method QueryHelper\CmsCategoryAclQueryField andFieldCmsCategoryId()
+ * @method QueryHelper\CmsCategoryAclQueryField orFieldCmsCategoryId()
+ * @method CmsCategoryAclQuery orderAscCmsCategoryId()
+ * @method CmsCategoryAclQuery orderDescCmsCategoryId()
+ * @method CmsCategoryAclQuery groupByCmsCategoryId()
+ * @method QueryHelper\CmsCategoryAclQueryField whereAccess()
+ * @method QueryHelper\CmsCategoryAclQueryField andFieldAccess()
+ * @method QueryHelper\CmsCategoryAclQueryField orFieldAccess()
+ * @method CmsCategoryAclQuery orderAscAccess()
+ * @method CmsCategoryAclQuery orderDescAccess()
+ * @method CmsCategoryAclQuery groupByAccess()
+ * @method QueryHelper\CmsCategoryAclQueryField andField($fieldName, $tableName = null)
+ * @method QueryHelper\CmsCategoryAclQueryField where($fieldName, $tableName = null)
+ * @method QueryHelper\CmsCategoryAclQueryField orField($fieldName, $tableName = null)
+ * @method QueryHelper\CmsCategoryAclQueryJoin join($tableName, $targetTableName = null, $alias = null)
+ * @method QueryHelper\CmsCategoryAclQueryJoin joinLeft($tableName, $targetTableName = null, $alias = null)
+ * @method CmsCategoryAclRecord[] find()
+ * @method CmsCategoryAclRecord findFirst()
+ * @method CmsCategoryAclRecord findPk($value)
+ */
+//</editor-fold>
+class CmsCategoryAclQuery extends \Mmi\Orm\Query {
+
+	protected $_tableName = 'cms_category_acl';
+
+}
---
+++ src/Cms/Orm/QueryHelper/CmsCategoryAclQueryField.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Cms\Orm\QueryHelper;
+
+/**
+ * @method \Cms\Orm\CmsCategoryAclQuery equals($value)
+ * @method \Cms\Orm\CmsCategoryAclQuery notEquals($value)
+ * @method \Cms\Orm\CmsCategoryAclQuery greater($value)
+ * @method \Cms\Orm\CmsCategoryAclQuery less($value)
+ * @method \Cms\Orm\CmsCategoryAclQuery greaterOrEquals($value)
+ * @method \Cms\Orm\CmsCategoryAclQuery lessOrEquals($value)
+ * @method \Cms\Orm\CmsCategoryAclQuery like($value)
+ * @method \Cms\Orm\CmsCategoryAclQuery ilike($value)
+ * @method \Cms\Orm\CmsCategoryAclQuery between($from, $to)
+ * @method \Cms\Orm\CmsCategoryAclQuery equalsColumnId()
+ * @method \Cms\Orm\CmsCategoryAclQuery notEqualsColumnId()
+ * @method \Cms\Orm\CmsCategoryAclQuery greaterThanColumnId()
+ * @method \Cms\Orm\CmsCategoryAclQuery lessThanColumnId()
+ * @method \Cms\Orm\CmsCategoryAclQuery greaterOrEqualsColumnId()
+ * @method \Cms\Orm\CmsCategoryAclQuery lessOrEqualsColumnId()
+ * @method \Cms\Orm\CmsCategoryAclQuery equalsColumnCmsRoleId()
+ * @method \Cms\Orm\CmsCategoryAclQuery notEqualsColumnCmsRoleId()
+ * @method \Cms\Orm\CmsCategoryAclQuery greaterThanColumnCmsRoleId()
+ * @method \Cms\Orm\CmsCategoryAclQuery lessThanColumnCmsRoleId()
+ * @method \Cms\Orm\CmsCategoryAclQuery greaterOrEqualsColumnCmsRoleId()
+ * @method \Cms\Orm\CmsCategoryAclQuery lessOrEqualsColumnCmsRoleId()
+ * @method \Cms\Orm\CmsCategoryAclQuery equalsColumnCmsCategoryId()
+ * @method \Cms\Orm\CmsCategoryAclQuery notEqualsColumnCmsCategoryId()
+ * @method \Cms\Orm\CmsCategoryAclQuery greaterThanColumnCmsCategoryId()
+ * @method \Cms\Orm\CmsCategoryAclQuery lessThanColumnCmsCategoryId()
+ * @method \Cms\Orm\CmsCategoryAclQuery greaterOrEqualsColumnCmsCategoryId()
+ * @method \Cms\Orm\CmsCategoryAclQuery lessOrEqualsColumnCmsCategoryId()
+ * @method \Cms\Orm\CmsCategoryAclQuery equalsColumnAccess()
+ * @method \Cms\Orm\CmsCategoryAclQuery notEqualsColumnAccess()
+ * @method \Cms\Orm\CmsCategoryAclQuery greaterThanColumnAccess()
+ * @method \Cms\Orm\CmsCategoryAclQuery lessThanColumnAccess()
+ * @method \Cms\Orm\CmsCategoryAclQuery greaterOrEqualsColumnAccess()
+ * @method \Cms\Orm\CmsCategoryAclQuery lessOrEqualsColumnAccess()
+ */
+class CmsCategoryAclQueryField extends \Mmi\Orm\QueryHelper\QueryField {
+
+}
