fbf146b42d71678135eece78a6b4b7fa5e7ff708
rpdiss <noopek@gmail.com>
2017-10-27 12:18:16 +0200

    no message

--- src/CmsAdmin/Grid/Column/OperationColumn.php
+++ src/CmsAdmin/Grid/Column/OperationColumn.php
@@ -126,24 +126,24 @@ class OperationColumn extends ColumnAbstract
                     continue;
                 }
                 //html przycisku
-                $html .= '<a href="' . $view->url($params) . rtrim('#' . $button['hashTarget'], '#') . '"><i class="fa fa-' . $button['iconName'] . ' icon-' . $button['iconName'] . '"></i></a>&nbsp;&nbsp;';
+                $html .= '<a class="operation-button" href="' . $view->url($params) . rtrim('#' . $button['hashTarget'], '#') . '"><i class="fa fa-' . $button['iconName'] . ' icon-' . $button['iconName'] . '"></i></a>';
             }
         }
         //link edycyjny ze sprawdzeniem ACL
         if (!empty($editParams) && (new AclAllowed)->aclAllowed($params = $this->_parseParams($editParams, $record))) {
-            $html .= '<a href="' . $view->url($params) . rtrim('#' . $this->getOption('editHashTarget'), '#') . '"><i class="fa fa-2 fa-edit "></i></a>&nbsp;&nbsp;';
+            $html .= '<a class="operation-button" href="' . $view->url($params) . rtrim('#' . $this->getOption('editHashTarget'), '#') . '"><i class="fa fa-2 fa-edit "></i></a>';
         }
         //link kasujący ze sprawdzeniem ACL
         if (!empty($deleteParams) && (new AclAllowed)->aclAllowed($params = $this->_parseParams($deleteParams, $record))) {
-            $html .= '<a href="' . $view->url($params) . rtrim('#' . $this->getOption('deleteHashTarget'), '#') . '" title="Czy na pewno usunąć" class="confirm"><i class="fa fa-2 fa-trash-o "></i></a>&nbsp;&nbsp;';
+            $html .= '<a class="operation-button" href="' . $view->url($params) . rtrim('#' . $this->getOption('deleteHashTarget'), '#') . '" title="Czy na pewno usunąć" class="confirm"><i class="fa fa-2 fa-trash-o "></i></a>';
         }
         //link kasujący tag
         if (!empty($deleteTagParams)) {
             if ($record->getJoined('cms_tag_relation')->id) {
-                $html .= '<a href="' . $view->url($this->_parseParams($deleteTagParams, $record)) . '" title="Tag jest przypisany do zasobu. Jeżeli zostanie usunięty nie ma możliwości przywrócenia relacji. Czy na pewno usunąć" class="confirm red"><i class="fa fa-2 fa-trash-o "></i></a>&nbsp;&nbsp;';
+                $html .= '<a class="operation-button" href="' . $view->url($this->_parseParams($deleteTagParams, $record)) . '" title="Tag jest przypisany do zasobu. Jeżeli zostanie usunięty nie ma możliwości przywrócenia relacji. Czy na pewno usunąć" class="confirm red"><i class="fa fa-2 fa-trash-o "></i></a>';
             }
             if (!$record->getJoined('cms_tag_relation')->id) {
-                $html .= '<a href="' . $view->url($this->_parseParams($deleteTagParams, $record)) . '" title="Czy na pewno usunąć" class="confirm"><i class="fa fa-2 fa-trash-o "></i></a>&nbsp;&nbsp;';
+                $html .= '<a class="operation-button" href="' . $view->url($this->_parseParams($deleteTagParams, $record)) . '" title="Czy na pewno usunąć" class="confirm"><i class="fa fa-2 fa-trash-o "></i></a>';
             }
         }
         return $html;
