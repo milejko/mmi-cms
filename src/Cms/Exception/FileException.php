5f76e5bb12f1830c171c190cd01cec67d38c1c0c
Mariusz Miłejko <mariusz@milejko.pl>
2017-11-20 17:39:53 +0100

    cached binary thumbs

---
+++ src/Cms/Exception/FileException.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Mmi Framework (https://github.com/milejko/mmi.git)
+ * 
+ * @link       https://github.com/milejko/mmi.git
+ * @copyright  Copyright (c) 2010-2017 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
+ */
+
+namespace Cms\Exception;
+
+/**
+ * Klasa wyjątku plikowego
+ */
+class FileException extends \Mmi\App\KernelException
+{
+    
+}
