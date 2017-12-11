e63b4c41f8e3eb4ac0d6145116c00cfaaa452f5e
Mariusz Miłejko <mariusz@milejko.pl>
2017-12-11 15:26:12 +0100

    drop delete button category grid

--- src/CmsAdmin/Plugin/CategoryGrid.php
+++ src/CmsAdmin/Plugin/CategoryGrid.php
@@ -33,7 +33,7 @@ class CategoryGrid extends \CmsAdmin\Grid\Grid
             ->setMultioptions((new \Cms\Orm\CmsCategoryQuery)->orderAscUri()->findPairs('uri', 'uri'))
             ->setFilterMethodLike()
             ->setLabel('okruszki'));
-
+        
         //uri
         $this->addColumn((new Column\TextColumn('customUri'))
             ->setLabel('inny adres'));
@@ -42,10 +42,6 @@ class CategoryGrid extends \CmsAdmin\Grid\Grid
         $this->addColumn((new Column\TextColumn('title'))
             ->setLabel('meta tytuł'));
 
-        //follow
-        $this->addColumn((new Column\CheckboxColumn('follow'))
-            ->setLabel('wyszukiwarki'));
-
         //aktywności
         $this->addColumn((new Column\CheckboxColumn('active'))
             ->setLabel('włączona'));
@@ -53,7 +49,7 @@ class CategoryGrid extends \CmsAdmin\Grid\Grid
         //operacje
         $this->addColumn((new Column\CustomColumn('operation'))
             ->setLabel('<div style="width: 55px;color: #20a8d8; text-align: center;"><i class="fa fa-2 fa-gears"></i></div>')
-            ->setTemplateCode('{$id = $record->id}{if categoryAclAllowed($id)}<a href="{@module=cmsAdmin&controller=category&action=edit&id={$id}@}" id="category-edit-{$id}"><i class="fa fa-2 fa-edit"></i></a>&nbsp;&nbsp;<a href="{@module=cmsAdmin&controller=category&action=delete&id={$id}@}" title="Czy na pewno usunąć" class="confirm" id="category-delete-{$id}"><i class="fa fa-2 fa-trash-o"></i></a>{else}-{/if}')
+            ->setTemplateCode('{$id = $record->id}{if categoryAclAllowed($id)}<a href="{@module=cmsAdmin&controller=category&action=edit&id={$id}@}" id="category-edit-{$id}"><i class="fa fa-2 fa-edit"></i></a>{else}-{/if}')
         );
             
     }
