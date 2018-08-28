07e0750d75ea97bb4406f7e55dc0edc9ca3c74c3
Mariusz Miłejko <m.milejko@nowaera.pl>
2018-08-28 18:47:35 +0200

    grid date fix

--- src/CmsAdmin/Resource/template/grid/filter/datetime.tpl
+++ src/CmsAdmin/Resource/template/grid/filter/datetime.tpl
@@ -2,7 +2,7 @@
     {$_value = php_explode(';', $_column->getFilterValue())}
     <div class="input-group grid-range">
         <span class="input-group-addon">od</span>
-        <input class="form-control grid-picker from no-focus"
+        <input class="form-control grid-picker from"
                type="datetime"
                name="{$_column->getFormColumnName()}-from"
                class="text"
@@ -10,7 +10,7 @@
                autocomplete="off"
                value="{$_value[0]}">
         <span class="input-group-addon">do</span>
-        <input class="form-control grid-picker to no-focus"
+        <input class="form-control grid-picker to"
                type="datetime"
                name="{$_column->getFormColumnName()}-to"
                class="text"
@@ -18,5 +18,5 @@
                autocomplete="off"
                value="{php_isset($_value[1]) ? $_value[1] : ''}">
     </div>
-    <input type="hidden" class="grid-filter hidden" name="{$_column->getFormColumnName()}" data-method="{$_column->getOption('method')}" value="{$_column->getFilterValue()}">
+    <input type="hidden" class="hidden" name="{$_column->getFormColumnName()}" data-method="{$_column->getOption('method')}" value="{$_column->getFilterValue()}">
 {/if}
--- src/CmsAdmin/Resource/web/js/grid.js
+++ src/CmsAdmin/Resource/web/js/grid.js
@@ -1,16 +1,24 @@
 var CMS = CMS ? CMS : {};
-var selectedPosition = 0,
-    selectedInput = null,
-    filtering = false;
 
 CMS.grid = function () {
     "use strict";
+
+    var inputCursorPosition = 0,
+        inputName,
+        inputBuffer,
+        filtering = false;
 
     var quickSwitch = function(data) {
         $('.grid-anchor').html(data.body);
         $('.paginator-anchor').html(data.paginator);
         initPaginator();
         initPicker();
+    };
+
+    var rememberCursor = function(object) {
+        inputBuffer = object.val();
+        inputCursorPosition = object[0].selectionStart;
+        inputName = object.attr('name');
     };
 
     var initPicker = function () {
@@ -22,22 +30,25 @@ CMS.grid = function () {
         var filter = field.attr('name'),
             value = field.val();
         filtering = true;
+        field.parent('div').parent('th').parent('tr').parent('thead').find('input.grid-picker').prop('disabled', true);
         $.ajax({
             url: window.location,
             type: 'POST',
             data: {filter: filter, value: value},
             success: function (data) {
                 quickSwitch(data);
-                if (!selectedInput) {
+                if (!inputName) {
                     filtering = false;
                     return;
                 }
-                var element = $('input[name=\'' + selectedInput + '\']');
+                var element = $('input[name=\'' + inputName + '\']');
                 element.focus();
+                element.val(inputBuffer);
                 try {
-                    element[0].setSelectionRange(selectedPosition, selectedPosition);
+                    element[0].setSelectionRange(inputCursorPosition, inputCursorPosition);
                 } catch (e) {}
-                selectedInput = null;
+                inputName = null;
+                inputBuffer = null;
                 filtering = false;
                 return true;
             }
@@ -45,64 +56,55 @@ CMS.grid = function () {
     };
 
     var initPaginator = function () {
-        if($('.paginator-anchor ul.pagination > li.page-item > a').length) {
-            $('.paginator-anchor ul.pagination > li.page-item > a').on('click', function (evt) {
-                var filter = $('ul.pagination').data('name'),
-                    value = $(this).data('page'),
-                    gridId = $('table').attr("id");
-                $.ajax({
-                    url: window.location,
-                    type: 'POST',
-                    data: {filter: filter, value: value},
-                    success: function (data) {
-                        quickSwitch(data);
-                    }
-                });
-            });
+        if(!$('.paginator-anchor ul.pagination > li.page-item > a').length) {
+            return;
         }
+        var filter = $('ul.pagination').data('name'),
+            value = $(this).data('page');
+        $.ajax({
+            url: window.location,
+            type: 'POST',
+            data: {filter: filter, value: value},
+            success: function (data) {
+                quickSwitch(data);
+            }
+        });
     };
 
     var initGridFilter = function () {
-        $('table.table-striped').on('keyup', ".grid-filter", function (event) {
-            selectedPosition = $(this)[0].selectionStart;
-            selectedInput = $(this).attr('name');
+        $('table.grid-anchor').on('keyup', ".grid-filter", function (event) {
+            rememberCursor($(this));
         });
 
-        $('table.table-striped').on('change', ".grid-range > input", function () {
-            if (!$(this).hasClass('no-focus')) {
-                selectedPosition = $(this)[0].selectionStart;
-                selectedInput = $(this).attr('name');
-            }
+        $('table.grid-anchor').on('change', ".grid-range > input", function () {
             var from = $(this).parent('div.input-group').children('input.from').val();
             var to = $(this).parent('div.input-group').children('input.to').val();
             $(this).parent('div.input-group').parent('div.form-group').children('input.hidden').val(from + ';' + to);
-            $(this).parent('div.input-group').children('input').prop('disabled', true);
             if (!filtering) {
                 filter($(this).parent('div.input-group').parent('div.form-group').children('input.hidden'));
             }
         });
 
-        $('table.table-striped').on('change', ".grid-filter", function () {
+        $('table.grid-anchor').on('change', ".grid-filter", function () {
             if (!filtering) {
                 filter($(this));
             }
         });
 
-        $('table.table-striped').on('focus', ".grid-filter", function () {
-            selectedInput = $(this).attr('name');
+        $('table.grid-anchor').on('click', ".grid-filter", function () {
+            rememberCursor($(this));
         });
 
-        $('table.table-striped').on('focus', "a.order", function () {
-            selectedInput = null;
+        $('table.grid-anchor').on('focus', "a.order", function () {
+            inputName = null;
         });
 
     };
 
     var initGridOrder = function () {
         //sortowanie grida
-        $('table.table-striped').on('click', 'th > div.form-group > a.order', function () {
+        $('table.grid-anchor').on('click', 'th > div.form-group > a.order', function () {
             var field = $(this).attr('href'),
-                gridId = $(this).parent().parent().parent().parent().parent().parent().find('table').attr("id"),
                 method = $(this).attr('data-method');
             $.ajax({
                 url: window.location,
@@ -118,15 +120,12 @@ CMS.grid = function () {
 
     var initGridOperation = function () {
         //akcja na zmianie checkboxa
-        $('table.table-striped').on('change', 'td > label.control-checkbox > input.checkbox', function () {
+        $('table.grid-anchor').on('change', 'td > label.control-checkbox > input.checkbox', function () {
             var id = $(this).attr('id').split('-');
             $.ajax({
                 url: window.location,
                 type: 'POST',
-                data: {id: id[1], name: id[0], value: $(this).val(), checked: $(this).is(':checked')},
-                success: function() {
-
-                }
+                data: {id: id[1], name: id[0], value: $(this).val(), checked: $(this).is(':checked')}
             });
         });
     };
