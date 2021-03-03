0654957590604e85313dd3e22755f393359fadbe
Mariusz Miłejko <mariusz@milejko.pl>
2021-03-03 08:36:41 +0100

    fix container getter

--- src/CmsAdmin/DbController.php
+++ src/CmsAdmin/DbController.php
@@ -12,7 +12,7 @@ namespace CmsAdmin;
 
 use Mmi\Mvc\Controller;
 use Ifsnop\Mysqldump\Mysqldump;
-use Mmi\App\App;
+use App\Registry;
 use Psr\Container\ContainerInterface;
 
 /**
@@ -36,8 +36,8 @@ class DbController extends Controller
     public function dumpAction()
     {
         ob_end_clean();
-        //TODO: remove after upgrade
-        $this->container = App::$di;
+        //TODO: remove after upgrade MMi to 4.x
+        $this->container = Registry::$di;
         //new dumper object
         $dumper = new Mysqldump(
             'mysql:host=' . $this->container->get('db.host') . ';dbname=' . $this->container->get('db.name') . ';dbport=' . $this->container->get('db.port'),
