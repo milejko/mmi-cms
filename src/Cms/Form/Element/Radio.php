cb79e69877f676996187708c9e6fb13fb434b099
Patryk Gałązka <pgalazka@users.noreply.github.com>
2021-06-14 16:13:39 +0200

    Fix/multicheckbox no disc (#82)
    * no list style on multicheckbox
    
    * type

--- src/Cms/Form/Element/Radio.php
+++ src/Cms/Form/Element/Radio.php
@@ -2,7 +2,7 @@
 
 /**
  * Mmi Framework (https://github.com/milejko/mmi.git)
- * 
+ *
  * @link       https://github.com/milejko/mmi.git
  * @copyright  Copyright (c) 2010-2017 Mariusz Miłejko (mariusz@milejko.pl)
  * @license    https://en.wikipedia.org/wiki/BSD_licenses New BSD License
@@ -33,8 +33,8 @@ class Radio extends \Mmi\Form\Element\Radio
      */
     public function __construct($name)
     {
-        $this->addClass('form-control');
         parent::__construct($name);
+        $this->addClass('form-check-input');
     }
 
 }
