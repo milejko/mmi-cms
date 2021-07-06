fe888836ceb69420d82f6840daa3fab34425e6b3
Patryk Gałązka <pgalazka@users.noreply.github.com>
2021-07-06 11:14:25 +0200

    validator for at least n files (#89)
    * validator for at least n files
    
    * validator for at least n files
    
    * validator for at least n files
    
    * validator for at most n files

---
+++ src/Cms/Validator/MinNumberCmsFiles.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Mmi Framework (https://github.com/milejko/mmi.git)
+ *
+ * @link       https://github.com/milejko/mmi.git
+ * @copyright  Copyright (c) 2010-2016 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
+ */
+
+namespace Cms\Validator;
+
+/**
+ * Walidator - minimalna liczba plików w Cms (musi być conajmniej tyle ile podano)
+ *
+ * @method self setValue($value) ustawia wartość bazową
+ * @method self setObject($object) ustawia obiekt
+ * @method self setObjectId($objectId) ustawia ID obiektu
+ * @method self setClass($class) ustawia klasę plików
+ * @method self setActive($active) ustawia aktywność
+ * @method self setTemporary($tmp) ustawia, czy pliki tymczasowe
+ * @method self setMessage($message) ustawia własną wiadomość walidatora
+ *
+ * @method string getValue() pobiera wartość bazową
+ * @method string getObject() pobiera obiekt
+ * @method integer getObjectId() pobiera ID obiektu
+ * @method string getClass() pobiera klasę plików
+ * @method boolean getActive() pobiera aktywność
+ * @method boolean getTemporary() pobiera, czy pliki tymczasowe
+ * @method string getMessage() pobiera wiadomość
+ */
+class MinNumberCmsFiles extends \Mmi\Validator\ValidatorAbstract
+{
+    /**
+     * Komunikat błędnego kodu zabezpieczającego
+     */
+    const INVALID = 'validator.minNumberCmsFiles.message';
+
+    /**
+     * Ustawia opcje
+     * @param array $options
+     * @return self
+     */
+    public function setOptions(array $options = [], $reset = false)
+    {
+        return $this->setValue(current($options))
+            ->setObject(next($options))
+            ->setObjectId(next($options))
+            ->setMessage(next($options));
+    }
+
+    /**
+     * Waliduje czy w cms_file znajduje się wystarczająca liczba plików dla danego rekordu
+     * @param mixed $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        $query = (new \Cms\Orm\CmsFileQuery)
+            ->byObject($this->_getObjectName(), $this->getObjectId())
+            ->andFieldSize()->notEquals(null);
+        if ($this->getClass()) {
+            $query->andFieldClass()->equals($this->getClass());
+        }
+        if ($this->getActive() !== null) {
+            $query->andFieldActive()->equals($this->getActive());
+        }
+        if ($query->count() < $this->getValue()) {
+            $this->_error(self::INVALID);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Zwraca nazwę obiektu dla zapytania o pliki
+     * @return string
+     */
+    protected function _getObjectName()
+    {
+        if ($this->getTemporary() === false) {
+            return $this->getObject();
+        }
+        return 'tmp-' . $this->getObject();
+    }
+}
