04251d4ed2bcd8b92e3bb15f1400436cd1f421ae
Patryk Gałązka <pgalazka@users.noreply.github.com>
2021-07-23 09:58:40 +0200

    Fix/multiple multifield conflict (#96)
    * multiple multifield conflict
    
    * multiple multifield conflict
    
    * multiple multifield conflict
    
    * multiple multifield css
    
    * multiple multifield css
    
    * multiple multifield css
    
    * multiple multifield css
    
    * multiple multifield
    
    * multiple multifield
    
    * multiple multifield
    
    * multiple multifield
    
    * multiple multifield
    
    * multiple multifield
    
    * multiple multifield
    
    * multiple multifield
    
    * multiple multifield
    
    * multiple multifield
    
    * multiple multifield

--- src/Cms/Form/Element/MultiField.php
+++ src/Cms/Form/Element/MultiField.php
@@ -11,11 +11,12 @@
 namespace Cms\Form\Element;
 
 use Mmi\Form\Element\ElementAbstract;
+use Mmi\Form\Form;
 
 /**
  * Element wielokrotny checkbox
  */
-class MultiField extends \Mmi\Form\Element\ElementAbstract
+class MultiField extends ElementAbstract
 {
     //szablon początku pola
     const TEMPLATE_BEGIN = 'cmsAdmin/form/element/element-abstract/begin';
@@ -27,7 +28,7 @@ class MultiField extends \Mmi\Form\Element\ElementAbstract
     const TEMPLATE_ERRORS = 'cmsAdmin/form/element/element-abstract/errors';
     //szablon etykiety
     const TEMPLATE_LABEL = 'cmsAdmin/form/element/element-abstract/label';
-
+    //pliki js i css
     const JQUERY_URL         = '/resource/cmsAdmin/js/jquery/jquery.js';
     const MULTIFIELD_JS_URL  = '/resource/cmsAdmin/js/multifield.js';
     const MULTIFIELD_CSS_URL = '/resource/cmsAdmin/css/multifield.css';
@@ -62,11 +63,11 @@ class MultiField extends \Mmi\Form\Element\ElementAbstract
     /**
      * Ustawia form macierzysty
      *
-     * @param \Mmi\Form\Form $form
+     * @param Form $form
      *
      * @return self
      */
-    public function setForm(\Mmi\Form\Form $form)
+    public function setForm(Form $form): self
     {
         parent::setForm($form);
 
@@ -82,33 +83,78 @@ class MultiField extends \Mmi\Form\Element\ElementAbstract
      *
      * @return boolean
      */
-    public function isValid()
+    public function isValid(): bool
     {
         $result = true;
-        if (!is_array($this->getValue())) {
+        if (false === is_array($this->getValue())) {
             return $result;
         }
+
         foreach ($this->getValue() as $index => $itemValues) {
-            foreach ($this->getElements() as $element) {
-                $value = $itemValues[$element->getBaseName()] ?? null;
-                //waliduje poprawnie jeśli niewymagane, ale tylko gdy niepuste
-                if (empty($value) && false === $element->getRequired()) {
-                    continue;
-                }
-                //iteracja po walidatorach
-                foreach ($element->getValidators() as $validator) {
-                    if ($validator->isValid($value)) {
-                        continue;
-                    }
-                    $result = false;
-                    //dodawanie wiadomości z walidatora
-                    $this->_elementErrors[$index][$element->getBaseName()][] = $validator->getMessage() ? $validator->getMessage() : $validator->getError();
-                }
-            }
+            $this->validateItem($index, $itemValues, $result);
         }
 
         //zwrot rezultatu wszystkich walidacji (iloczyn)
         return $result;
+    }
+
+    /**
+     * @param int   $index
+     * @param array $itemValues
+     * @param bool  $result
+     */
+    private function validateItem(int $index, array $itemValues, bool &$result): void
+    {
+        foreach ($this->getElements() as $element) {
+            $value = $itemValues[$element->getBaseName()] ?? null;
+
+            //waliduje zagnieżdżone pole multifield
+            if ($element instanceof self) {
+                $this->validateMultifieldElement($element, $value, $result);
+                continue;
+            }
+
+            //waliduje poprawnie jeśli niewymagane, ale tylko gdy niepuste
+            if (empty($value) && false === $element->getRequired()) {
+                continue;
+            }
+
+            //iteracja po walidatorach
+            $this->validateElement($index, $element, $value, $result);
+        }
+    }
+
+    /**
+     * @param ElementAbstract $element
+     * @param array|null      $value
+     * @param bool            $result
+     */
+    private function validateMultifieldElement(ElementAbstract $element, ?array $value, bool &$result): void
+    {
+        $element->setValue($value);
+
+        if (false === $element->isValid()) {
+            $result = false;
+        }
+    }
+
+    /**
+     * @param int               $index
+     * @param ElementAbstract   $element
+     * @param array|string|null $value
+     * @param bool              $result
+     */
+    private function validateElement(int $index, ElementAbstract $element, array|string|null $value, bool &$result): void
+    {
+        foreach ($element->getValidators() as $validator) {
+            if ($validator->isValid($value)) {
+                continue;
+            }
+
+            $result = false;
+            //dodawanie wiadomości z walidatora
+            $this->_elementErrors[$index][$element->getBaseName()][] = $validator->getMessage() ? $validator->getMessage() : $validator->getError();
+        }
     }
 
     /**
@@ -118,7 +164,7 @@ class MultiField extends \Mmi\Form\Element\ElementAbstract
      *
      * @return self
      */
-    public function addElement(ElementAbstract $element)
+    public function addElement(ElementAbstract $element): self
     {
         //ustawianie opcji na elemencie
         if ($this->_form) {
@@ -134,7 +180,7 @@ class MultiField extends \Mmi\Form\Element\ElementAbstract
      *
      * @return ElementAbstract[]
      */
-    final public function getElements()
+    final public function getElements(): array
     {
         return $this->_elements;
     }
@@ -142,7 +188,7 @@ class MultiField extends \Mmi\Form\Element\ElementAbstract
     /**
      * @return string
      */
-    public function fetchField()
+    public function fetchField(): string
     {
         $this->view->headScript()->prependFile(self::JQUERY_URL);
         $this->view->headScript()->appendScript($this->jsScript());
@@ -162,7 +208,7 @@ class MultiField extends \Mmi\Form\Element\ElementAbstract
     /**
      * @return string
      */
-    private function renderList()
+    private function renderList(): string
     {
         $html = '<ul class="list-unstyled field-list">';
 
@@ -188,7 +234,7 @@ class MultiField extends \Mmi\Form\Element\ElementAbstract
      *
      * @return string
      */
-    private function renderListElement(?array $itemValues = null, string $index = '**')
+    private function renderListElement(?array $itemValues = null, string $index = '**'): string
     {
         $html = '<li class="field-list-item border mb-3 p-3">
             <a href="#" class="btn-toggle" role="button">
@@ -198,7 +244,7 @@ class MultiField extends \Mmi\Form\Element\ElementAbstract
 
         foreach ($this->getElements() as $element) {
             $element->setId($this->getId() . '-' . $index . '-' . $element->getBaseName());
-            $element->setName($this->getBaseName() . '[' . $index . '][' . $element->getBaseName() . ']');
+            $element->setName($this->getName() . '[' . $index . '][' . $element->getBaseName() . ']');
             $element->setValue($itemValues[$element->getBaseName()] ?? null);
             $element->setErrors($this->_elementErrors[$index][$element->getBaseName()] ?? []);
 
@@ -221,7 +267,7 @@ class MultiField extends \Mmi\Form\Element\ElementAbstract
     /**
      * @return string
      */
-    private function jsScript()
+    private function jsScript(): string
     {
         $listElement = addcslashes($this->renderListElement(), "'");
         $listId      = $this->getId() . '-list';
