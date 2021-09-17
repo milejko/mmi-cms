85d21db2c773fb34a04e155027663c1389e919a0
Mariusz Miłejko <mariusz@milejko.pl>
2021-09-17 15:06:43 +0200

    multiple queries fix

---
+++ src/Cms/Orm/QueryHelper/CmsTagRelationQueryJoin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Cms\Orm\QueryHelper;
+
+/**
+ * @method \Cms\Orm\CmsTagRelationQuery on($localKeyName, $joinedKeyName = 'id')
+ */
+class CmsTagRelationQueryJoin extends \Mmi\Orm\QueryHelper\QueryJoin
+{
+
+}
