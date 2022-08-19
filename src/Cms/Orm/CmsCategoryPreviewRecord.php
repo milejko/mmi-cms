6eb849fd5ef909ab6152835f8b0c274068694701
Mariusz Miłejko <mariusz@milejko.pl>
2022-08-19 14:30:30 +0200

    preview fix (#151)

---
+++ src/Cms/Orm/CmsCategoryPreviewRecord.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Cms\Orm;
+
+/**
+ * Rekord podglądu kategorii CMSowych
+ */
+class CmsCategoryPreviewRecord extends CmsCategoryRecord
+{
+    private CmsCategoryRecord $originalCmsCategoryRecord;
+
+    public function setFromCmsCategoryRecord(CmsCategoryRecord $cmsCategoryRecord): self
+    {
+        $this->setFromArray($cmsCategoryRecord->toArray());
+        if ($this->cmsCategoryOriginalId) {
+            $this->originalCmsCategoryRecord = (new CmsCategoryQuery)->findPk($this->cmsCategoryOriginalId);
+        }
+        return $this;
+    }
+
+    public function getChildrenRecords()
+    {
+        if (null === $this->originalCmsCategoryRecord) {
+            return parent::getChildrenRecords();
+        }
+        return $this->originalCmsCategoryRecord->getChildrenRecords();
+    }
+
+    public function getSiblingsRecords()
+    {
+        if (null === $this->originalCmsCategoryRecord) {
+            return parent::getSiblingsRecords();
+        }
+        return $this->originalCmsCategoryRecord->getChildrenRecords();
+    }
+
+}
